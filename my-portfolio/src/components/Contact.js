2d11bb91e7b4458530bf55eaadba19669918d4b4
Magoon18 <denverdavis1209@gmail.com>
2024-09-08 15:45:42 -0700

    Changes in Contact section

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -27,8 +27,8 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        {/*<div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          {/*<iframe
             width="100%"
             height="100%"
             title="map"
@@ -38,16 +38,20 @@ export default function Contact() {
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=csulb&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
+          />*/}
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 24 24">
+              <path
+                d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+            </svg>
+            <a href="https://www.linkedin.com/in/denver-davis-5b2ab7256/" target="_blank" className="ml-2 mr-6 hover:text-white">
+              @denverdavis
+            </a>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
@@ -62,7 +66,7 @@ export default function Contact() {
               <p className="leading-relaxed">(909)552-4299</p>
             </div>
           </div>
-        </div>*/}
+        </div>
         <form
           netlify
           name="contact"
