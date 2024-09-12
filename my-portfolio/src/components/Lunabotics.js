e15fce2b96cbb6a3b84c579726ddf99eb8ab333c
Magoon18 <denverdavis1209@gmail.com>
2024-09-11 23:33:10 -0700

    Updated videos

--- my-portfolio/src/components/Lunabotics.js
+++ my-portfolio/src/components/Lunabotics.js
@@ -30,7 +30,7 @@ export default function Lunabotics() {
                   width = "750" height = "500" controls>
                   <source src={project.video} type="video/mp4"/>
                 </video>*/}
-                *<div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                {/*<div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
@@ -38,9 +38,27 @@ export default function Lunabotics() {
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
-                </div>
+                </div>*/}
               </div>
             </a>
+          ))}
+          {luna.map((project) => (
+            <a
+              href={project.link}
+              key={project.video}
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
+                {/*<img
+                  alt="gallery"
+                  className="absolute inset-0 size-fit object-cover object-center"
+                  src={project.image}
+                />*/}
+                <video
+                  width = "750" height = "500" controls>
+                  <source src={project.video} type="video/mp4"/>
+                </video>
+              </div>
+            </a> 
           ))}
         </div>
       </div>
