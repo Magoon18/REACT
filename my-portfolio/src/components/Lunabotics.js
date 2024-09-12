3ba29e75c22d5cdd66052db25ef22e9875535c1b
Magoon18 <denverdavis1209@gmail.com>
2024-09-11 22:50:59 -0700

    video

--- my-portfolio/src/components/Lunabotics.js
+++ my-portfolio/src/components/Lunabotics.js
@@ -27,10 +27,9 @@ export default function Lunabotics() {
                   src={project.image}
                 />
                 <video
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.video} type="video/mp4"
-                />
+                  width = "750" height = "500" controls>
+                  <source src={project.video} type="video/mp4"/>
+                </video>
                 *<div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
