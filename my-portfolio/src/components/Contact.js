85ce9372cf58ed795150796d4f227c07e16b359d
Magoon18 <denverdavis1209@gmail.com>
2024-09-18 15:18:40 -0700

    Took out phone number

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -60,10 +60,10 @@ export default function Contact() {
               <a className="text-indigo-400 leading-relaxed">
                 Denverdavis1209@gmail.com
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+              {/*<h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">(909)552-4299</p>
+              <p className="leading-relaxed">(909)552-4299</p>*/}
             </div>
           </div>
         </div>
